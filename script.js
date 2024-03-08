23b818a08163fc2a3f2478b08a0fd0ec70c80136
Th3Manuel <162655771+Th3Manuel@users.noreply.github.com>
2024-03-07 23:10:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.getElementById('siBtn').addEventListener('click', function() {
 
     // Mostrar el mensaje específico
     document.getElementById('messageContainer').style.display = 'block';
-    document.getElementById('messageContainer').innerHTML = '¡De igual forma ya lo somos :3';
+    document.getElementById('messageContainer').innerHTML = '¡Gracias:3';
 
     // Mostrar otro gif después de 3 segundos
     setTimeout(function() {
